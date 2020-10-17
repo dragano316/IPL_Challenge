ce7a39c43d66d9e0a4fbcc3a98a0145e12a02dfc
Divyank Mishra <42536880+dragano316@users.noreply.github.com>
2020-10-18 03:08:21 +0530

    Updates

---
+++ precache-manifest.650859058b37a6a9114567bc8b468296.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1755c1d97c387ae372f5f8fda2050d1",
+    "url": "/IPL_Challenge/index.html"
+  },
+  {
+    "revision": "91cb1aac6238822f2c1c",
+    "url": "/IPL_Challenge/static/css/main.78045737.chunk.css"
+  },
+  {
+    "revision": "b012ffc4a14365ea3a26",
+    "url": "/IPL_Challenge/static/js/2.5c7772e6.chunk.js"
+  },
+  {
+    "revision": "20e0b5784b832842af656bfe4339af38",
+    "url": "/IPL_Challenge/static/js/2.5c7772e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91cb1aac6238822f2c1c",
+    "url": "/IPL_Challenge/static/js/main.777938d8.chunk.js"
+  },
+  {
+    "revision": "444a94d3d512595c31f1",
+    "url": "/IPL_Challenge/static/js/runtime-main.e8369749.js"
+  },
+  {
+    "revision": "96f5d1a1792bf12ce281dacbd588a089",
+    "url": "/IPL_Challenge/static/media/Ball_by_Ball.96f5d1a1.csv"
+  },
+  {
+    "revision": "9fc576264568a655d7e15eb8135df344",
+    "url": "/IPL_Challenge/static/media/Match.9fc57626.csv"
+  },
+  {
+    "revision": "ff8fbde8622b0c55cacac7e7671e3d77",
+    "url": "/IPL_Challenge/static/media/Player.ff8fbde8.csv"
+  },
+  {
+    "revision": "1315c877adb302667e001e2ec0f98b95",
+    "url": "/IPL_Challenge/static/media/Season.1315c877.csv"
+  },
+  {
+    "revision": "40c9d1d34375c96699082e89a2d6c729",
+    "url": "/IPL_Challenge/static/media/atlanlogo.40c9d1d3.svg"
+  }
+]);
